23f4a688ab08ae812f07fe03020785176fa9f399
ttaufik1903 <54972223+ttaufik1903@users.noreply.github.com>
2021-01-11 11:17:16 +0700

    xxx

---
+++ src/main/java/com/example/redis/springbootrediscache/controller/xxx.java
@@ -0,0 +1,5 @@
+package com.example.redis.springbootrediscache.controller;
+
+public class xxx {
+
+}
---
+++ src/main/java/com/example/redis/springbootrediscache/model/Employee.java
@@ -0,0 +1,24 @@
+package com.example.redis.springbootrediscache.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+}
